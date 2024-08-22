ce490850e67928609a5358cdcfc4660e01b8d3fc
Giovanni Arellano <35707883+giovanniare@users.noreply.github.com>
2024-08-22 00:00:45 -0600

    Git updates changes (#5)
    * Adding update conditons and dev mode
    
    * update conditions part 2

--- run.py
+++ run.py
@@ -1,10 +1,23 @@
-from code_updates.git_process import GitHandler 
+from code_updates.git_process import GitHandler
+from code_updates.cmd_line_args import CmdLineArgs
 
 
 if __name__ == "__main__":
-    git = GitHandler()
-    git.checkout_all()
-    git.pull()
+    cmd_line = CmdLineArgs()
+    cmd_line.initialize()
+    cmd_line.build()
+
+    args = cmd_line.args
+    if not args.dev:
+        git = GitHandler()
+        git.checkout_all()
+        branch = args.branch
+        if branch:
+            git.switch_to_brach(branch)
+        else:
+            git.set_main()
+
+        git.pull()
 
     # run application
     from app import App
